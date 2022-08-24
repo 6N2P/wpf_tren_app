983fc08cd84f2b1c5fdca840e4dc21d2e13d75b6
6N2P <umnik1985@mail.ru>
2022-08-24 23:06:28 +0500

    добавил БД. создал класс Атлета. Подключил ентети фреимВерк

---
+++ WpfTrenApp/Data/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace WpfTrenApp.Data
+{
+    internal class AppContext : DbContext
+    {
+        public DbSet<Atlet> Atlets { get; set; }
+
+        public AppContext() : base("DefaultConnection") { }
+    }
+}
---
+++ WpfTrenApp/Data/Atlet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTrenApp.Data
+{
+    internal class Atlet
+    {
+        //название переменных должно быть такоеже как в БД это важно!!!
+        private int ID { get; set; }
+        private string Name { get; set; }
+        private int Age { get; set; }
+        private int Growth { get; set; }
+        private int Weight { get; set; }
+        private string Progtren { get; set; }
+
+        public Atlet() { }
+
+        public Atlet(int iD, string name, int age, int growth, int weight, string progtren)
+        {
+            ID = iD;
+            Name = name;
+            Age = age;
+            Growth = growth;
+            Weight = weight;
+            Progtren = progtren;
+        }
+    }
+}
